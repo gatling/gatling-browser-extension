ec1bcd7ac5252fbbbd08c93d5406aad56b72079d
Gauthier POULET <gauthier.poulet@gmail.com>
2023-03-13 17:12:26 +0100

    add interHtmlResources (step4), RND-17

---
+++ src/utils/generate/inferHtmlResources.ts
@@ -0,0 +1,47 @@
+import { JSDOM } from "jsdom";
+
+import { type GroupedRequest } from "@src/interfaces/Request";
+
+export const getResourcesFromHtml = (body: string): string[] => {
+  const dom = new JSDOM(body);
+  const links = Array.from(dom.window.document.querySelectorAll("link")).map(
+    (link) => link.href
+  );
+  const scripts = Array.from(
+    dom.window.document.querySelectorAll("script")
+  ).map((script) => script.src);
+  const images = Array.from(dom.window.document.querySelectorAll("img")).map(
+    (img) => img.src
+  );
+  return [...links, ...images, ...scripts].filter(Boolean);
+};
+
+const filterResourcesFromHtml = (
+  contentText: string,
+  request: GroupedRequest
+): GroupedRequest => {
+  const resources = getResourcesFromHtml(contentText);
+  const resourcesWithHostname = resources.map((r) =>
+    !r.startsWith("http://") || !r.startsWith("https://")
+      ? new URL(request.root.request.url).origin + r
+      : r
+  );
+  request.resources.forEach((resource) =>
+    console.log("resource.request.url", resource.request.url)
+  );
+  request.resources = request.resources.filter(
+    (resource) => !resourcesWithHostname.includes(resource.request.url)
+  );
+  return request;
+};
+
+export const interResources = (
+  groupedRequests: GroupedRequest[]
+): GroupedRequest[] => {
+  return groupedRequests.map((request) => {
+    const contentText = request.root.response.content.text;
+    return contentText
+      ? filterResourcesFromHtml(contentText, request)
+      : request;
+  });
+};
